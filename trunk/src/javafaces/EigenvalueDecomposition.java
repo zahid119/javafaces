7dda4075509baf5677845c9028a4301fde542cc8
sajanjoseph <sajanjoseph@9940ce80-a9d0-11dd-9b86-41075523de00>
2008-11-04 18:23:20 +0000

    unversioned java files

---
+++ trunk/src/javafaces/EigenvalueDecomposition.java
@@ -0,0 +1,20 @@
+package javafaces;
+
+public class EigenvalueDecomposition extends cern.colt.matrix.linalg.EigenvalueDecomposition{
+	public EigenvalueDecomposition(Matrix2D dmat){
+		super(dmat);
+	}
+	public double[]getEigenValues(){
+		return diag(getD().toArray());
+	}
+	public double[][] getEigenVectors(){
+		return getV().toArray();
+	}
+	private double[] diag(double[][] m) {
+	    double[] d = new double[m.length];
+	    for (int i = 0; i< m.length; i++)
+	      d[i] = m[i][i];
+	    return d;
+	}
+	
+}
